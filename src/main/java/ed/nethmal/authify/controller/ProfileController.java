6a508490978a04786c68df9a8e17c4f3d3560de8
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-11 22:48:59 +0530

    vaid adding

--- src/main/java/ed/nethmal/authify/controller/ProfileController.java
+++ src/main/java/ed/nethmal/authify/controller/ProfileController.java
@@ -3,6 +3,7 @@ package ed.nethmal.authify.controller;
 import ed.nethmal.authify.io.ProfileRequest;
 import ed.nethmal.authify.io.ProfileResponse;
 import ed.nethmal.authify.service.ProfileService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class ProfileController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public ProfileResponse register(@RequestBody ProfileRequest request) {
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
         return profileService.createProfile(request);
     }
 
--- src/main/java/ed/nethmal/authify/io/ProfileRequest.java
+++ src/main/java/ed/nethmal/authify/io/ProfileRequest.java
@@ -1,13 +1,21 @@
 package ed.nethmal.authify.io;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 
 @Data
 @AllArgsConstructor
 public class ProfileRequest {
 
+    @NotBlank(message = "Name should be not empty")
     private String name;
+
+    @Email(message = "Enter valid email")
+    @NotNull(message = "Email should be not empty")
     private String email;
+
+    @Size(min = 6, message = "Password should be at least 6 characters")
     private String password;
 }
