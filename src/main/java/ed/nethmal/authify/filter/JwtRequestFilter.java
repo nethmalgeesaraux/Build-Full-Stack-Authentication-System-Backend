e803481369e3de99524a22f023d898b7928dbdbc
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-13 21:31:02 +0530

    create JwtRequestFilter

---
+++ src/main/java/ed/nethmal/authify/filter/JwtRequestFilter.java
@@ -0,0 +1,88 @@
+package ed.nethmal.authify.filter;
+
+import ed.nethmal.authify.service.AppUserDetailsService;
+import ed.nethmal.authify.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final AppUserDetailsService appUserDetailsService;
+    private final JwtUtil jwtUtil;
+
+    private static final List<String> EXCLUDE_URLS = List.of(
+            "/api/login",
+            "/api/profile/register",
+            "/api/send-reset-otp",
+            "/api/reset-password",
+            "/api/logout"
+    );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getServletPath();
+        if (EXCLUDE_URLS.contains(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = null;
+        String email = null;
+
+        // 1. check the authorization header
+        final String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+        }
+
+        // 2. If not found in header, check cookies
+        if (jwt == null) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("jwt".equals(cookie.getName())) {
+                        jwt = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+        }
+
+        // 3. validate the token and set security context
+        if (jwt != null) {
+            email = jwtUtil.extractEmail(jwt);
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = appUserDetailsService.loadUserByUsername(email);
+                if (jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
+
+
+        filterChain.doFilter(request, response);
+    }
+}
