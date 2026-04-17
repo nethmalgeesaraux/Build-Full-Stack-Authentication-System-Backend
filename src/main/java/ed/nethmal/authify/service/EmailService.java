5523d33132c00404592d7d4ee95e07b5aefd6bb0
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-17 15:11:34 +0530

    create EmailService message

---
+++ src/main/java/ed/nethmal/authify/service/EmailService.java
@@ -0,0 +1,26 @@
+package ed.nethmal.authify.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to Our Platform");
+        message.setText("Hello " + name + ",\n\nThanks for registering with us!\n\nRegards,\n\nAuthify Team");
+    }
+
+}
