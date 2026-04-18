7a66fb27bc955b523ae6f6588ec419df51f99fd9
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-18 15:14:24 +0530

    logout api

--- src/main/java/ed/nethmal/authify/controller/AuthController.java
+++ src/main/java/ed/nethmal/authify/controller/AuthController.java
@@ -81,5 +81,18 @@ public class AuthController {
         return ResponseEntity.ok(email != null);
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(Map.of("message", "Logged out successfully"));
+    }
+
 
 }
